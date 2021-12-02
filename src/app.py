c3cf294bd54316da0071ee28b8fb940e4da86837
Eugene Zuev <zhekazuev@gmail.com>
2021-12-02 18:45:46 +0100

    add flask app - get data from api and render html

---
+++ src/app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+def check_mailex(number, headers, language="EN", add_post_office_info="true"):
+    """Use api checkmailex and get json with information about parcel by number
+
+    Args:
+        number (str): parcel number, f.e. 12345 or CP12345PL
+        headers (dict): dict with headers from browser
+        language (str): language of response - EN or PL
+        add_post_office_info (str): add or not info about post office to response - true or false
+
+    Returns:
+        json: json with information about parcel
+    """
+    # api url from emonitoring site
+    url = 'https://uss.poczta-polska.pl/uss/v1.0/tracking/checkmailex'
+    
+    # standard api key from poland post site
+    API_KEY = "BiGwVG2XHvXY+kPwJVPA8gnKchOFsyy39Thkyb1wAiWcKLQ1ICyLiCr"\
+        "xj1+vVGC+kQk3k0b74qkmt5/qVIzo7lTfXhfgJ72Iyzz05wH2XZI6AgXVDciX"\
+        "7G2jLCdoOEM6XegPsMJChiouWS2RZuf3eOXpK5RPl8Sy4pWj+b07MLg=.Mjg0"\
+        "Q0NFNzM0RTBERTIwOTNFOUYxNkYxMUY1NDZGMTA0NDMwQUIyRjg4REUxMjk5N"\
+        "DAyMkQ0N0VCNDgwNTc1NA==.b24415d1b30a456cb8ba187b34cb6a86"
+
+    # add headers for request
+    headers.update({
+        "Content-Type": "application/json; charset=utf-8",
+        "API_KEY": API_KEY,
+    })
+
+    # add parameters to query string
+    query_string = {"language":language,"number":number,"addPostOfficeInfo":add_post_office_info}
+
+    response = requests.post(url, json=query_string, headers=headers)
+    data = response.json()
+    return data
+
+@app.route('/')
+def index():
+    """Main function 
+    - get and check parcel number
+    - add headers to next request 
+    - checks
+    - render html - jinja
+
+    Returns:
+        str: rendered html based on index.html template
+    """
+    # get parcel number argument
+    number = request.args.get('number')
+
+    # add headers
+    headers = {}
+    headers['User-Agent'] = request.headers.get('User-Agent')
+    headers['Cookie'] = request.headers.get('Cookie')
+    headers['Accept-Encoding'] = request.headers.get('Accept-Encoding')
+    headers['Accept-Language'] = request.headers.get('Accept-Language')
+
+    # check number in get-request
+    if number:
+        data = check_mailex(number, headers)
+    else:
+        data = None
+
+    # check status of parcel - if mailStatus == -1 - parcel doesn't exist, else - exist
+    if data.get('mailStatus') == -1:
+        data = None
+
+    return render_template('index.html', data=data)
+
+
+if __name__ == '__main__':
+    app.run()
